27d3c6c9c3c031a4e68c88301105832d8eddb94b
Chris Langfield <34426450+chris-langfield@users.noreply.github.com>
2021-06-02 18:46:20 -0400

    get FEAT version, load initial settings

--- FSF.py
+++ FSF.py
@@ -1,4 +1,58 @@
+import os
+import re
 
-class EV:
-    def __init__(self, title, basicShape, ):
-        pass
+# get $HOME
+
+HOME = os.getenv("HOME")
+print(HOME)
+
+# get $FSLDIR
+FSLDIR = os.getenv("FSLDIR")
+if FSLDIR:
+    print("$FSLDIR:",FSLDIR)
+else:
+    print("[!] Could not find environment variable $FSLDIR. Is FSL installed?")
+    exit()
+
+## get FEAT version
+FEATLIB_PATH = os.path.join(FSLDIR, "src/feat5/featlib.tcl")
+FEAT_VERSION = ""
+with open(FEATLIB_PATH,"r") as FEATLIB:
+    lines = [line for line in FEATLIB.readlines() if "set fmri(version)" in line]
+for line in lines:
+    try:
+        float(line.split()[-1])
+        FEAT_VERSION = line.split()[-1]
+    except ValueError:
+        continue
+
+print("FEAT version:", FEAT_VERSION)
+
+## get default settings
+DEFAULT_SETTINGS_PATH = os.path.join(FSLDIR,"etc/fslconf/feat.tcl")
+if not os.path.exists(DEFAULT_SETTINGS_PATH):
+    print("Warning: default FEAT settings ($FSLDIR/etc/fslconf/feat.tcl does not exist. Defaults will not be loaded.")
+
+class FeatSettings:
+    def __init__(self, defaultsFilename=DEFAULT_SETTINGS_PATH):
+        self.settings = {}
+        # FEAT version
+        self.settings["version"] = FEAT_VERSION
+        # get default settings
+        if os.path.exists(defaultsFilename):
+            with open(defaultsFilename) as defaults:
+                lines = [line for line in defaults.readlines() if not line.startswith("#") and line.startswith("set")]
+                for line in lines:
+                    option = re.search('set fmri\((.*)\)', line).group(1)
+                    value = line.split()[-1]
+                    self.settings[option] = value
+        else:
+            print("Warning: The defaults file specified (" + defaultsFilename + ") was not found. No settings were loaded.")
+
+    def printSettings(self):
+        for option in self.settings:
+            print(option,'--',self.settings[option])
+
+myFSF = FeatSettings()
+myFSF.printSettings()
+
