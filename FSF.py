f249059c7cad350e174295d7fa8af84409e4579e
Chris Langfield <34426450+chris-langfield@users.noreply.github.com>
2021-06-02 20:59:36 -0400

    finished data section

--- FSF.py
+++ FSF.py
@@ -40,22 +40,37 @@ class FeatLevel:
     FIRST_LEVEL = 1
     HIGHER_LEVEL = 2
 
+FeatLevelToStr = {
+    FeatLevel.FIRST_LEVEL:"First-level analysis",
+    FeatLevel.HIGHER_LEVEL:"Higher-level analysis"
+}
+
 class FeatStages:
-    FULL_ANALYSIS = 1
-    PREPROCESSING = 2
-    STATS = 3
+    FULL_ANALYSIS = 7
+    PREPROCESSING = 1
+    STATS = 2
+
+FeatStagesToStr = {
+    FeatStages.STATS:"Stats",
+    FeatStages.PREPROCESSING:"Pre-processing",
+    FeatStages.FULL_ANALYSIS:"Full analysis"
+}
 
 class FeatHigherLevelInput:
     FEAT_DIRS = 1
     COPE_IMAGES = 2
 
 class FeatSettings:
-    def __init__(self, defaultsFilename=DEFAULT_SETTINGS_PATH, LEVEL, ANALYSIS):
+    def __init__(self, LEVEL, ANALYSIS, defaultsFilename=DEFAULT_SETTINGS_PATH):
+
+        self.settings = {}
+        self.inputs = []
+
         # must be set
         self.LEVEL = LEVEL
         self.ANALYSIS = ANALYSIS
-
-        self.settings = {}
+        self.settings["level"] = LEVEL
+        self.settings["analysis"] = ANALYSIS
 
         # FEAT version
         self.settings["version"] = FEAT_VERSION
@@ -77,7 +92,7 @@ class FeatSettings:
         else:
             print("Only first-level and higher-level analyses are allowed")
     def setAnalysis(self, ANALYSIS):
-        if ANALYSIS in [1,2,3]:
+        if ANALYSIS in [1,2,7]:
             self.ANALYSIS = ANALYSIS
         else:
             print("Only full analysis, preprocessing, and stats are allowed")
@@ -94,22 +109,63 @@ class DataOptions:
     def __init__(self, myFeatSettings):
         self.parent = myFeatSettings
 
-    def Configure(self, outputDirectory, numInputs, inputPaths, totalVolumes=-1, deleteVolumes = -1, tr =-1, highPassCutoff = 60, higherLevelInput = FeatHigherLevelInput.FEAT_DIRS):
+    def Configure(self, outputDirectory, numInputs, inputPaths, totalVolumes=-1, deleteVolumes = -1, tr =-1, highPassCutoff = 60, higherLevelInput = FeatHigherLevelInput.COPE_IMAGES):
         if self.parent.LEVEL == FeatLevel.FIRST_LEVEL:
             if not tr == -1:
                 print("TR specified by user. Will not get TR from input image")
             else:
                 tr = subprocess.getoutput([FSLDIR + "/bin/fslval " + inputPaths[0] + " pixdim4"])
+                self.parent.settings["tr"] = float(tr.replace("\n","").strip())
                 print("TR is ", tr.replace("\n","").strip())
             if not totalVolumes == -1:
                 print("Number of volumes specified by user. Will not get TR from input image")
             else:
                 totalVolumes = subprocess.getoutput([FSLDIR + "/bin/fslnvols " + inputPaths[0]])
+                self.parent.settings["npts"] = int(totalVolumes.replace("\n","").strip())
                 print("Total volumes are", totalVolumes.replace("\n","").strip())
-
+            if not deleteVolumes == -1:
+                self.parent.settings["ndelete"] = deleteVolumes
+            self.parent.settings["paradigm_hp"] = highPassCutoff
 
         elif self.parent.LEVEL == FeatLevel.HIGHER_LEVEL:
-            pass
+            self.parent.settings["inputtype"] = higherLevelInput
+            self.parent.settings["npts"] = len(inputPaths)
 
+        self.parent.settings["outputdir"] = outputDirectory
+        self.parent.settings["multiple"] = numInputs
+
+        # input paths
+
+        for i in range(len(inputPaths)):
+            self.parent.inputs.append(inputPaths[i])
+
+
+        ## voxel size
+        dim1 = int(subprocess.getoutput([FSLDIR + "/bin/fslval " + inputPaths[0] + " dim1"]).replace("\n", "").strip())
+        dim2 = int(
+            subprocess.getoutput([FSLDIR + "/bin/fslval " + inputPaths[0] + " dim1"]).replace("\n", "").strip())
+        dim3 = int(
+            subprocess.getoutput([FSLDIR + "/bin/fslval " + inputPaths[0] + " dim1"]).replace("\n", "").strip())
+        dim4 = int(
+            subprocess.getoutput([FSLDIR + "/bin/fslval " + inputPaths[0] + " dim1"]).replace("\n", "").strip())
+
+        totalVoxels = dim1 * dim2 * dim3 * dim4
+        self.parent.settings["totalVoxels"] = totalVoxels
+
+
+
+    def printSettings(self):
+        print("Data Settings: " + self.parent.settings["outputdir"])
+        print(FeatLevelToStr[self.parent.LEVEL], "|", FeatStagesToStr[self.parent.ANALYSIS])
+        print("--------------")
+        print("Number of inputs:", self.parent.settings["multiple"])
+        print("Total volumes:", self.parent.settings["npts"])
+        print("TR:", self.parent.settings["tr"])
+        print("High pass filter cutoff:", self.parent.settings["paradigm_hp"])
+        if self.parent.LEVEL == FeatLevel.HIGHER_LEVEL:
+            print("Input type: (1=lower level feat directories, 2=lower level cope images)", self.parent.settings["inputtype"])
+        print("Inputs: ")
+        for i in range(len(self.parent.inputs)):
+            print("\t", i+1, self.parent.inputs[i])
 
 
