25442a33be1c854c0b4ff7e44c79302de7d1e71a
Chris Langfield <34426450+chris-langfield@users.noreply.github.com>
2021-06-03 14:29:07 -0400

    moving enum things to a new file

--- FSF.py
+++ FSF.py
@@ -1,6 +1,7 @@
 import os
 import re
 import subprocess
+from FSFLabels import *
 import fsl
 
 class PyFSFError(Exception):
@@ -49,53 +50,6 @@ if not os.path.exists(DEFAULT_SETTINGS_PATH):
     print("Warning: default FEAT settings ($FSLDIR/etc/fslconf/feat.tcl does not exist. Defaults will not be loaded.")
 
 
-
-class FeatLevel:
-    FIRST_LEVEL = 1
-    HIGHER_LEVEL = 2
-
-FeatLevelToStr = {
-    FeatLevel.FIRST_LEVEL:"First-level analysis",
-    FeatLevel.HIGHER_LEVEL:"Higher-level analysis"
-}
-
-class FeatAnalysis:
-    FULL_ANALYSIS = 7
-    PREPROCESSING = 1
-    STATS = 2
-
-FeatAnalysisToStr = {
-    FeatAnalysis.STATS:"Stats",
-    FeatAnalysis.PREPROCESSING:"Pre-processing",
-    FeatAnalysis.FULL_ANALYSIS:"Full analysis"
-}
-
-class FeatSliceTiming:
-    NONE = 0
-    REGULAR_UP = 1
-    REGULAR_DOWN = 2
-    ORDER_FILE = 3
-    TIMING_FILE = 4
-    INTERLEAVED = 5
-    Options = [NONE, REGULAR_UP, REGULAR_DOWN, ORDER_FILE, TIMING_FILE, INTERLEAVED]
-
-class FeatPerfusion:
-    FirstTimepointIsTag = 0
-    FirstTimepointIsControl = 1
-    Options = [FirstTimepointIsTag, FirstTimepointIsControl]
-
-class FeatUnwarp:
-    X_MINUS = "x-"
-    X_PLUS = "x+"
-    Y_MINUS = "y-"
-    Y_PLUS = "y+"
-    Z_MINUS = "z-"
-    Z_PLUS = "z+"
-    Directions = [X_PLUS,X_MINUS,Y_PLUS,Y_MINUS,Z_PLUS,Z_MINUS]
-
-class FeatHigherLevelInput:
-    FEAT_DIRS = 1
-    COPE_IMAGES = 2
 
 class FeatSettings:
     def __init__(self, LEVEL, # int
---
+++ FSFLabels.py
@@ -0,0 +1,53 @@
+
+
+class FeatLevel:
+    FIRST_LEVEL = 1
+    HIGHER_LEVEL = 2
+
+FeatLevelToStr = {
+    FeatLevel.FIRST_LEVEL:"First-level analysis",
+    FeatLevel.HIGHER_LEVEL:"Higher-level analysis"
+}
+
+class FeatAnalysis:
+    FULL_ANALYSIS = 7
+    PREPROCESSING = 1
+    STATS = 2
+
+FeatAnalysisToStr = {
+    FeatAnalysis.STATS:"Stats",
+    FeatAnalysis.PREPROCESSING:"Pre-processing",
+    FeatAnalysis.FULL_ANALYSIS:"Full analysis"
+}
+
+class FeatSliceTiming:
+    NONE = 0
+    REGULAR_UP = 1
+    REGULAR_DOWN = 2
+    ORDER_FILE = 3
+    TIMING_FILE = 4
+    INTERLEAVED = 5
+    Options = [NONE, REGULAR_UP, REGULAR_DOWN, ORDER_FILE, TIMING_FILE, INTERLEAVED]
+
+class FeatPerfusion:
+    FirstTimepointIsTag = 0
+    FirstTimepointIsControl = 1
+    Options = [FirstTimepointIsTag, FirstTimepointIsControl]
+
+class FeatUnwarp:
+    X_MINUS = "x-"
+    X_PLUS = "x+"
+    Y_MINUS = "y-"
+    Y_PLUS = "y+"
+    Z_MINUS = "z-"
+    Z_PLUS = "z+"
+    Directions = [X_PLUS,X_MINUS,Y_PLUS,Y_MINUS,Z_PLUS,Z_MINUS]
+
+class FeatHigherLevelInput:
+    FEAT_DIRS = 1
+    COPE_IMAGES = 2
+
+class RegistrationSearch:
+    NO_SEARCH = 0
+    NORMAL_SEARCH = 90
+    FULL_SEARCH = 180
